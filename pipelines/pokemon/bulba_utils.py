a901534bae39dfd64c161975b3cce3eb6635a4c6
Evelyn <evelyn.ciara.345@gmail.com>
2026-02-04 18:38:34 -0500

    add method to get wikitext

--- pipelines/pokemon/bulba_utils.py
+++ pipelines/pokemon/bulba_utils.py
@@ -4,8 +4,22 @@ Utility functions for extracting Bulbapedia content
 
 from urllib.parse import urlencode
 
+import requests
+
 
 API_BASE_URL = "https://bulbapedia.bulbagarden.net/w/api.php"
+
+def bp_wikitext_api_params(page_title: str) -> dict:
+    """
+    This dict can optionally be passed to requests.get() as query params
+    """
+    return {
+        "action": "expandtemplates",
+        "text": "{{:" + page_title + "}}",
+        "prop": "wikitext",
+        "format": "json",
+    }
+
 
 def bp_wikitext_url(page_title: str) -> str:
     """
@@ -13,11 +27,15 @@ def bp_wikitext_url(page_title: str) -> str:
 
     More info: https://www.mediawiki.org/wiki/API:Expandtemplates
     """
-    query_string = urlencode({
-        "action": "expandtemplates",
-        "text": "{{:" + page_title + "}}",
-        "prop": "wikitext",
-        "format": "json",
-    })
+    query_string = urlencode(bp_wikitext_api_params(page_title))
 
     return API_BASE_URL + "?" + query_string
+
+
+def get_bp_wikitext(page_title: str) -> str:
+    api_url = bp_wikitext_url(page_title)
+    # MediaWiki output will be wrapped in JSON object
+    mw_output_response = requests.get(api_url)
+    # Parse JSON and get wikitext
+    mw_output_json = mw_output_response.json()
+    page_wikitext = mw_output_json["expandtemplates"]["wikitext"]
