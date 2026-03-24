3283c765485a3d9ce64beb28831567899e3642e5
Evelyn <evelyn.ciara.345@gmail.com>
2026-03-24 04:55:16 -0400

    Add function to extract mbz result sets into normalized DataFrames

--- pipelines/utils/musicbrainz_helpers.py
+++ pipelines/utils/musicbrainz_helpers.py
@@ -3,6 +3,10 @@ from pathlib import Path
 import tomllib
 
 import musicbrainzngs as mbz
+import pandas as pd
+
+
+type ResultSet = dict[str, list[dict]]
 
 
 def get_package_version():
@@ -27,3 +31,48 @@ def setup():
 
     # Set default rate limit (1 request per second)
     mbz.set_rate_limit(limit_or_interval=1.0, new_requests=1)
+
+
+def to_dataframes(result_set: ResultSet) -> dict[str, pd.DataFrame]:
+    # Extracts all list-based result sets into separate DataFrames, and normalizes
+    # nested objects into separate DataFrames with foreign key relationships.
+    # Currently, this function only handles the "release-list" result set and its nested
+    # "release-group" objects.
+    normalized_dfs: dict[str, pd.DataFrame] = {}
+
+    if "release-list" in result_set:
+        release_list = result_set["release-list"]
+        releases_df = pd.json_normalize(release_list, max_level=0)
+
+        # Rename primary key now
+        releases_df = releases_df.rename(columns={"id": "release_id"})
+
+        # Check if release groups were returned in this result set
+        if "release-group" in releases_df.columns:
+            has_release_group = (
+                releases_df["release-group"].apply(lambda x: isinstance(x, dict))
+            )
+
+            # Extract release groups into a separate DataFrame
+            release_groups_df = pd.json_normalize(
+                releases_df.loc[has_release_group, "release-group"]
+            ).drop_duplicates(subset=["id"])
+
+            release_groups_df = (
+                release_groups_df
+                .rename(columns={"id": "release_group_id"})
+                .rename(columns=lambda x: x.replace("-", "_"))
+            )
+            normalized_dfs["release_groups"] = release_groups_df
+
+            # Replace nested object with FK
+            releases_df["release_group_id"] = releases_df["release-group"].apply(
+                lambda x: x.get("id") if isinstance(x, dict) else pd.NA
+            )
+
+            releases_df = releases_df.drop(columns=["release-group"])
+
+        releases_df = releases_df.rename(columns=lambda x: x.replace("-", "_"))
+        normalized_dfs["releases"] = releases_df
+
+    return normalized_dfs
