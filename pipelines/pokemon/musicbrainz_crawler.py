1e230e2e923d0ed8a036b9b4be3b564ee6a35036
Evelyn <evelyn.ciara.345@gmail.com>
2026-03-24 00:56:49 -0400

    Move helper functions to utils.musicbrainz_helpers module

--- pipelines/pokemon/musicbrainz_crawler.py
+++ pipelines/pokemon/musicbrainz_crawler.py
@@ -1,9 +1,8 @@
 import logging
-import os
-from pathlib import Path
-import tomllib
 
 import musicbrainzngs as mbz
+
+import utils.musicbrainz_helpers as mbz_helpers
 
 logger = logging.getLogger(__name__)
 
@@ -11,32 +10,8 @@ logger = logging.getLogger(__name__)
 # logging.basicConfig(level=logging.DEBUG)
 
 
-def get_package_version():
-    # Read the package version from pyproject.toml to include in the MusicBrainz API
-    # user agent string (in accordance with the DRY principle).
-    path = Path("pyproject.toml")
-    with path.open("rb") as f:
-        data = tomllib.load(f)
-        version = data.get("project", {}).get("version")
-    return version
-
-
-def setup():
-    # Set up MusicBrainz API client with appropriate user agent and rate limiting.
-    # See https://musicbrainz.org/doc/MusicBrainz_API/Rate_Limiting for details.
-    # The queries made by this crawler do not require authentication.
-    package_version = get_package_version()
-    useragent_contact = os.getenv(
-        "PIPELINES_CRAWLER_CONTACT", default="https://github.com/veeara282/vgmcharts"
-    )
-    mbz.set_useragent("VGMCharts", package_version, contact=useragent_contact)
-
-    # Set default rate limit (1 request per second)
-    mbz.set_rate_limit(limit_or_interval=1.0, new_requests=1)
-
-
 def main():
-    setup()
+    mbz_helpers.setup()
 
     # TODO: Retrieve data on Pokémon-related releases, recordings, artists, and works
     # using the MusicBrainz API.
