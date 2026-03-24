d443c0d326290ffdf8aeb0b781930273f087a6df
Evelyn <evelyn.ciara.345@gmail.com>
2026-03-23 20:24:38 -0400

    Read package ver. from pyproject.toml for user-agent string

--- pipelines/pokemon/musicbrainz_crawler.py
+++ pipelines/pokemon/musicbrainz_crawler.py
@@ -1,16 +1,37 @@
+import logging
 import os
+from pathlib import Path
+import tomllib
 
 import musicbrainzngs as mbz
+
+logger = logging.getLogger(__name__)
+
+# Uncomment this line to show debug logs from musicbrainzngs
+# logging.basicConfig(level=logging.DEBUG)
+
+
+def get_package_version():
+    # Read the package version from pyproject.toml to include in the MusicBrainz API
+    # user agent string (in accordance with the DRY principle).
+    path = Path("pyproject.toml")
+    with path.open("rb") as f:
+        data = tomllib.load(f)
+        version = data.get("project", {}).get("version")
+    return version
 
 
 def setup():
     # Set up MusicBrainz API client with appropriate user agent and rate limiting.
     # See https://musicbrainz.org/doc/MusicBrainz_API/Rate_Limiting for details.
     # The queries made by this crawler do not require authentication.
+    package_version = get_package_version()
     useragent_contact = os.getenv(
         "PIPELINES_CRAWLER_CONTACT", default="https://github.com/veeara282/vgmcharts"
     )
-    mbz.set_useragent("VGMCharts", "0.1.0", contact=useragent_contact)
+    mbz.set_useragent("VGMCharts", package_version, contact=useragent_contact)
+
+    # Set default rate limit (1 request per second)
     mbz.set_rate_limit(limit_or_interval=1.0, new_requests=1)
 
 
